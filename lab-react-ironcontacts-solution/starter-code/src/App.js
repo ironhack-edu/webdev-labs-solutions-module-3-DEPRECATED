29e819b823d05a008364664a1d41b95e28dbc39f
sandrabosk <sandraboskovic@hotmail.com>
2019-10-04 15:26:53 -0400

    update neam od component ContactList in ironcontact lab

--- lab-react-ironcontacts-solution/starter-code/src/App.js
+++ lab-react-ironcontacts-solution/starter-code/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ContactList from './components/contact-list';
+import ContactList from './components/ContactList';
 class App extends Component {
   render() {
     return (
