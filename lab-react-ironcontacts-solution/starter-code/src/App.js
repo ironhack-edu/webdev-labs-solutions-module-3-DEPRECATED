08f3665aafc4f10ca8acc4a8c3c920133a37f67d
sandrabosk <sandraboskovic@hotmail.com>
2019-10-04 15:30:40 -0400

    update name of the component ContactList in ironcontact lab

--- lab-react-ironcontacts-solution/starter-code/src/App.js
+++ lab-react-ironcontacts-solution/starter-code/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ContactList from './components/ContactList';
+import ContactList from './components/ContactsList';
 class App extends Component {
   render() {
     return (
