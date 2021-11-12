d55872589fab883f8df7313b9d2f707c8dc53f20
sandrabosk <sandraboskovic@hotmail.com>
2021-11-11 20:18:06 -0500

    fix issue with creating new Set

--- lab-react-training-solution-classes/src/components/clickablepicture/ClickablePicture.js
+++ lab-react-training-solution-classes/src/components/clickablepicture/ClickablePicture.js
@@ -4,14 +4,13 @@ class ClickablePicture extends React.Component {
 
   state = {
     src: this.props.img,
-    defaultSrc: [...new Set(this.props.img)],
+    defaultSrc: this.props.img,
     clickedSrc: this.props.imgClicked,
     clicked: false,
   };
   
   
   handlePicChange = () => {
-    console.log("-=-=-=-= this.props.img: ", this.props.img)
     this.setState({
       src: this.state.clicked ? this.state.defaultSrc : this.state.clickedSrc,
       clicked: !this.state.clicked,
@@ -19,7 +18,6 @@ class ClickablePicture extends React.Component {
   };
 
   render() {
-    // console.log("this.state.src: ", this.state.src)
     return (
       <div>
         <img src={this.state.src} onClick={this.handlePicChange} />
